4ea3655bf22a6e0b57ef1b3c29940cbbd12f083b
fabriziorodriguez2 <fabrizio.rodriguez@correo.ucu.edu.uy>
2025-11-18 17:13:00 -0300

    la re puta mdre

--- db/arreglo_turnos.sql
+++ db/arreglo_turnos.sql
@@ -12,14 +12,6 @@ UPDATE turno SET hora_inicio = '13:00:00', hora_fin = '14:00:00' WHERE id_turno 
 INSERT IGNORE INTO turno (hora_inicio, hora_fin)
 VALUES ('14:00:00', '15:00:00'),('15:00:00', '16:00:00'),('16:00:00', '17:00:00'),('17:00:00', '18:00:00'),('18:00:00', '19:00:00'),('19:00:00', '20:00:00'),('20:00:00', '21:00:00'),('21:00:00', '22:00:00'),('22:00:00', '23:00:00');
 
-INSERT INTO participante (email, nombre, apellido, ci)
-VALUES
- ('ana.admin@ucu.edu.uy',   'admin', 'ANA',   12345668),
- ('pedro.admin@ucu.edu.uy', 'admin', 'PEDRO', 23456789),
- ('laura.admin@ucu.edu.uy', 'admin', 'LAURA', 34567890);
+INSERT INTO participante (email, nombre, apellido, ci) VALUES ('ana.admin@ucu.edu.uy',   'admin', 'ANA',   12345668), ('pedro.admin@ucu.edu.uy', 'admin', 'PEDRO', 23456789), ('laura.admin@ucu.edu.uy', 'admin', 'LAURA', 34567890); 
 
-INSERT INTO login (correo,contrasena)
-VALUES
- ('ana.admin@ucu.edu.uy',   'admin'),
- ('pedro.admin@ucu.edu.uy', 'admin'),
- ('laura.admin@ucu.edu.uy', 'admin');
+INSERT INTO login (correo,contrasena) VALUES ('ana.admin@ucu.edu.uy',   'admin'), ('pedro.admin@ucu.edu.uy', 'admin'), ('laura.admin@ucu.edu.uy', 'admin');
